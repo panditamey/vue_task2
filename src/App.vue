71ab34c22307e547194915bfc79581eb87262f55
Amey Pandit <amey.pandit@sportzinteractive.com>
2024-08-27 15:12:27 +0530

    Added TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,18 @@
 <template>
   <div id="app">
-    <h3 class="bg-primary text-white text-center m-2 p-2">
-      {{ name }}'s Todo List
-    </h3>
-    <div class="container-fluid" p-4>
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
-      </div>
+    <h3 class="bg-primary text-white text-center m-2 p-2">Todo List</h3>
 
-      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{ t.action }}</div>
-        <div class="col-2 text-center">
-          <input type="checkbox" v-model="t.done" class="form-check-input" />
-        </div>
-      </div>
-
-      <!-- Input box -->
-      <div class="row py-2">
-        <div class="col">
-          <input v-model="newItemText" class="form-control" />
-        </div>
-        <div class="col-2">
-          <button class="btn btn-primary" v-on:click="addNewItem">Add</button>
-        </div>
-      </div>
-    </div>
+    <Todo />
   </div>
 </template>
 
 <script>
+import Todo from "./components/TodoList.vue";
+
 export default {
   name: "App",
-  data() {
-    return {
-      name: "Amey",
-      tasks: [
-        { action: "Buy milk", done: true },
-        { action: "Clean bathroom", done: false },
-        { action: "Feed dog", done: false },
-        { action: "Learn Vue", done: true },
-      ],
-      hideCompleted: true,
-      newItemText: "",
-    };
-  },
-  computed: {
-    filteredTasks() {
-      return this.hideCompleted
-        ? this.tasks.filter((t) => !t.done)
-        : this.tasks;
-    },
-  },
-  methods: {
-    addNewItem() {
-      this.tasks.push({ action: this.newItemText, done: false });
-      this.newItemText = "";
-    },
+  components: {
+    Todo,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,35 +1,66 @@
-<template setup>
+<template>
   <div id="app">
-    <h1 class="bg-primary text-white text-center m-2 p-2">
-      {{ name }}'s' Todo List
-    </h1>
-
-    <div class="container-fluid p-4">
+    <h3 class="bg-primary text-white text-center m-2 p-2">
+      {{ name }}'s Todo List
+    </h3>
+    <div class="container-fluid" p-4>
       <div class="row">
         <div class="col font-weight-bold">Task</div>
         <div class="col-2 font-weight-bold">Done</div>
       </div>
-    </div>
 
-    <div class="row" v-for="t in tasks" v-bind:key="t.action">
-      <div class="col">{{ t.action }}</div>
-      <div class="col">{{ t.done }}</div>
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <div class="col">{{ t.action }}</div>
+        <div class="col-2 text-center">
+          <input type="checkbox" v-model="t.done" class="form-check-input" />
+        </div>
+        <button class="btn btn-primary" v-on:click="deleteItem">Delete</button>
+      </div>
+
+      <!-- Input box -->
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewItem">Add</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "App",
+  name: "Todo",
   data() {
     return {
+      name: "Amey",
       tasks: [
-        { action: "Buy Flowers", done: false },
-        { action: "Get doctor appoinment", done: false },
-        { action: "Collect Tickets", done: true },
-        { action: "Attend review meeting", done: false },
+        { action: "Buy milk", done: true },
+        { action: "Clean bathroom", done: false },
+        { action: "Feed dog", done: false },
+        { action: "Learn Vue", done: true },
       ],
+      hideCompleted: true,
+      newItemText: "",
     };
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted
+        ? this.tasks.filter((t) => !t.done)
+        : this.tasks;
+    },
+  },
+  methods: {
+    addNewItem() {
+      this.tasks.push({ action: this.newItemText, done: false });
+      this.newItemText = "";
+    },
+    deleteItem() {
+      this.tasks.pop();
+    },
   },
 };
 </script>
