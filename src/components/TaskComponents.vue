ebdb730d120e2ab3a4f79fb886c336b333e172ca
Amey Pandit <amey.pandit@sportzinteractive.com>
2024-08-27 14:50:46 +0530

    first commit

---
+++ src/components/TaskComponents.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>SignUp</h2>
+    {{ email }} {{ password }}
+    <p><input type="text" placeholder="Email" v-model="email" /></p>
+    <p><input type="password" placeholder="Password" v-model="password" /></p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
+<template>
+  <div>
+    <h2>SignUp</h2>
+    {{ email }} {{ password }}
+    <p><input type="text" placeholder="Email" v-model="email" /></p>
+    <p><input type="password" placeholder="Password" v-model="password" /></p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template setup>
+  <div id="app">
+    <h1 class="bg-primary text-white text-center m-2 p-2">
+      {{ name }}'s' Todo List
+    </h1>
+
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+    </div>
+
+    <div class="row" v-for="t in tasks" v-bind:key="t.action">
+      <div class="col">{{ t.action }}</div>
+      <div class="col">{{ t.done }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      tasks: [
+        { action: "Buy Flowers", done: false },
+        { action: "Get doctor appoinment", done: false },
+        { action: "Collect Tickets", done: true },
+        { action: "Attend review meeting", done: false },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
